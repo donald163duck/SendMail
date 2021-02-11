7099ff092f31d1ee9317630f97dc238bc16b36ce
alexander.podsevalov <alexpodrolla@gmail.com>
2021-02-11 10:33:45 +0400

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿
+namespace SendMail
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtmail = new System.Windows.Forms.TextBox();
+            this.txtsub = new System.Windows.Forms.TextBox();
+            this.txtmess = new System.Windows.Forms.TextBox();
+            this.btnsend = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(91, 45);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(90, 90);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Nirmala UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.label1.Location = new System.Drawing.Point(57, 150);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(159, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Send Email in C#";
+            // 
+            // txtmail
+            // 
+            this.txtmail.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(247)))), ((int)(((byte)(247)))));
+            this.txtmail.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtmail.Font = new System.Drawing.Font("New Century Schoolbook", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtmail.ForeColor = System.Drawing.SystemColors.ScrollBar;
+            this.txtmail.Location = new System.Drawing.Point(35, 189);
+            this.txtmail.Multiline = true;
+            this.txtmail.Name = "txtmail";
+            this.txtmail.Size = new System.Drawing.Size(202, 27);
+            this.txtmail.TabIndex = 2;
+            this.txtmail.Text = "Email";
+            this.txtmail.Enter += new System.EventHandler(this.txtmail_Enter);
+            this.txtmail.Leave += new System.EventHandler(this.txtmail_Leave);
+            // 
+            // txtsub
+            // 
+            this.txtsub.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(247)))), ((int)(((byte)(247)))));
+            this.txtsub.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtsub.Font = new System.Drawing.Font("New Century Schoolbook", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtsub.ForeColor = System.Drawing.SystemColors.ScrollBar;
+            this.txtsub.Location = new System.Drawing.Point(35, 231);
+            this.txtsub.Multiline = true;
+            this.txtsub.Name = "txtsub";
+            this.txtsub.Size = new System.Drawing.Size(202, 27);
+            this.txtsub.TabIndex = 2;
+            this.txtsub.Text = "Subject";
+            this.txtsub.Enter += new System.EventHandler(this.txtsub_Enter);
+            this.txtsub.Leave += new System.EventHandler(this.txtsub_Leave);
+            // 
+            // txtmess
+            // 
+            this.txtmess.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(247)))), ((int)(((byte)(247)))));
+            this.txtmess.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtmess.Font = new System.Drawing.Font("New Century Schoolbook", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtmess.ForeColor = System.Drawing.SystemColors.ScrollBar;
+            this.txtmess.Location = new System.Drawing.Point(35, 273);
+            this.txtmess.Multiline = true;
+            this.txtmess.Name = "txtmess";
+            this.txtmess.Size = new System.Drawing.Size(202, 81);
+            this.txtmess.TabIndex = 2;
+            this.txtmess.Text = "Message..";
+            this.txtmess.Enter += new System.EventHandler(this.txtmess_Enter);
+            this.txtmess.Leave += new System.EventHandler(this.txtmess_Leave);
+            // 
+            // btnsend
+            // 
+            this.btnsend.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnsend.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnsend.Font = new System.Drawing.Font("Nirmala UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnsend.ForeColor = System.Drawing.Color.White;
+            this.btnsend.Location = new System.Drawing.Point(35, 370);
+            this.btnsend.Name = "btnsend";
+            this.btnsend.Size = new System.Drawing.Size(202, 34);
+            this.btnsend.TabIndex = 3;
+            this.btnsend.Text = "Send Mail";
+            this.btnsend.UseVisualStyleBackColor = false;
+            this.btnsend.Click += new System.EventHandler(this.btnsend_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(257, 462);
+            this.Controls.Add(this.btnsend);
+            this.Controls.Add(this.txtmess);
+            this.Controls.Add(this.txtsub);
+            this.Controls.Add(this.txtmail);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtmail;
+        private System.Windows.Forms.TextBox txtsub;
+        private System.Windows.Forms.TextBox txtmess;
+        private System.Windows.Forms.Button btnsend;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Mail;
+
+namespace SendMail
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtmail_Enter(object sender, EventArgs e)
+        {
+            if (txtmail.Text == " Email")
+            {
+                txtmail.Clear();
+                txtmail.ForeColor = Color.FromArgb(83, 179, 233);
+            }
+        }
+
+        private void txtmail_Leave(object sender, EventArgs e)
+        {
+            if (txtmail.Text == "")
+            {
+                txtmail.ForeColor = Color.FromArgb(200, 200, 200);
+                txtmail.Text = " Email";
+            }
+        }
+
+        private void txtsub_Enter(object sender, EventArgs e)
+        {
+            if (txtsub.Text == " Subject")
+            {
+                txtsub.Clear();
+                txtsub.ForeColor = Color.FromArgb(83, 179, 233);
+            }
+        }
+
+        private void txtsub_Leave(object sender, EventArgs e)
+        {
+            if (txtsub.Text == "")
+            {
+                txtsub.ForeColor = Color.FromArgb(200, 200, 200);
+                txtsub.Text = " Subject";
+            }
+        }
+
+        private void txtmess_Enter(object sender, EventArgs e)
+        {
+            if (txtmess.Text == " Message")
+            {
+                txtmess.Clear();
+                txtmess.ForeColor = Color.FromArgb(83, 179, 233);
+            }
+        }
+
+        private void txtmess_Leave(object sender, EventArgs e)
+        {
+            if (txtmess.Text == "")
+            {
+                txtmess.ForeColor = Color.FromArgb(200, 200, 200);
+                txtmess.Text = " Subject";
+            }
+        }
+
+        private void btnsend_Click(object sender, EventArgs e)
+        {
+            string to, from, pass, messageBody;
+            MailMessage message = new MailMessage();
+            to = txtmail.Text;
+            from = "alexpodrolla@gmail.com";
+            pass = "Alexandr163!";
+            messageBody = txtmess.Text;
+            message.To.Add(to);
+            message.From = new MailAddress(from);
+            message.Body = "From : " + "<br>Message: " + messageBody;
+            message.Subject = txtsub.Text;
+            message.IsBodyHtml = true;
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com");
+            smtp.EnableSsl = true;
+            smtp.Port = 587;
+            smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtp.Credentials = new NetworkCredential(from, pass);
+
+            try
+            {
+                smtp.Send(message);
+                DialogResult code = MessageBox.Show("Email успешно отправлен!", "Email отправлен!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                if (code == DialogResult.OK)
+                {
+                    txtmail.Clear();
+                    txtsub.Clear();
+                    txtmess.Clear();
+
+                }
+            } 
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+    }
+}
